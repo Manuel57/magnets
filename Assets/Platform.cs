bb0020a245d0af87d8f4e20540a8e8192a3296d5
Manuel57 <manuel.lackenbucher@gmail.com>
2020-04-25 16:37:40 +0200

    magnets

---
+++ Assets/Platform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private bool movingPlatform;
+    [SerializeField] private float lowerBound;
+    [SerializeField] private float upperBound;
+    [SerializeField] private Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float delta = Time.deltaTime ;
+
+        if (movingPlatform)
+        {
+            if (Vector2.Dot(direction,transform.position) <= lowerBound || Vector2.Dot(direction, transform.position) >= upperBound)
+            {
+                direction = -direction;
+            }
+
+            transform.position = new Vector2(transform.position.x + direction.x * delta, transform.position.y + direction.y * delta);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public static int JumpSpeed = 30;
+
+    [SerializeField] private MagnetAction pos;
+    [SerializeField] private MagnetAction neg;
+    public Vector2 Movement { get; set; }
+
+
+    public bool allowJump = true;
+
+    public bool PosUp { get; set; } = true;
+    public bool PlatformTouched { get; set; } = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+        if (Movement == null || Movement.magnitude == 0)
+        {
+            return;
+        }
+        float delta = Time.deltaTime * 3;
+
+        transform.position = new Vector3(transform.position.x + Movement.x * delta, transform.position.y);
+
+
+        Movement = Vector2.zero;
+
+    }
+
+    public void Switch(Player other)
+    {
+        PosUp = !PosUp;
+    }
+
+    public void Force(Vector2 dir)
+    {
+        GetComponent<Rigidbody2D>().AddForce(dir, ForceMode2D.Impulse);
+    }
+
+
+    public void Move(Vector2 dir)
+    {
+        Movement = dir;
+    }
+
+    public void Left(Vector2 dir)
+    {
+        Movement = dir;
+    }
+
+    public void Left()
+    {
+        Left(Vector2.left);
+    }
+    public void Right(Vector2 dir)
+    {
+        Movement = dir;
+    }
+    public void Right()
+    {
+        Right(Vector2.right);
+    }
+
+    public void Jump(Player other)
+    {
+        if (allowJump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+            allowJump = false;
+            PlatformTouched = true;
+        }
+    }
+
+
+    private bool gt = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+
+        if (collision.collider.tag.Equals("platform"))
+        {
+            allowJump = true;
+        }
+
+
+
+    }
+
+}
